989abcd80d7c97258e3eb5ce4676f875df30127d
hwangjungpil <thinkhero@gmail.com>
2024-04-25 17:29:26 +0900

    first commit

---
+++ spring/KeycloakTest/src/main/java/com/ktds/KeycloakTest/OauthResourceServerConfiguration.java
@@ -0,0 +1,61 @@
+package com.ktds.KeycloakTest;
+
+import jakarta.servlet.Filter;
+import org.keycloak.adapters.authorization.spi.ConfigurationResolver;
+import org.keycloak.adapters.authorization.spi.HttpRequest;
+import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.keycloak.adapters.authorization.integration.jakarta.ServletPolicyEnforcerFilter;
+import org.keycloak.util.JsonSerialization;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class OauthResourceServerConfiguration {
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    String jwkSetUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests()
+//                    .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .addFilterBefore(createPolicyEnforcerFilter(), BearerTokenAuthenticationFilter.class);
+        return http.build();
+    }
+
+    private ServletPolicyEnforcerFilter createPolicyEnforcerFilter() {
+        PolicyEnforcerConfig config;
+
+        try {
+            config = JsonSerialization.readValue(getClass().getResourceAsStream("/policy-enforcer.json"), PolicyEnforcerConfig.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new ServletPolicyEnforcerFilter(new ConfigurationResolver() {
+            @Override
+            public PolicyEnforcerConfig resolve(HttpRequest httpRequest) {
+                return config;
+            }
+        });
+    }
+
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+    }
+}
