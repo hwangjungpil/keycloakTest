4a09f68793221f5b09b506bab0fc074297c2e419
hwangjungpil <thinkhero@gmail.com>
2024-05-13 17:40:38 +0900

    jwt parsing cors error

--- spring/KeycloakTestJwtParsing/src/main/java/com/ktds/KeycloakTest/controller/HelloController.java
+++ spring/KeycloakTestJwtParsing/src/main/java/com/ktds/KeycloakTest/controller/HelloController.java
@@ -1,14 +1,15 @@
-package com.ktds.KeycloakTest;
+package com.ktds.KeycloakTest.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8082/")
-public class Controller {
+public class HelloController {
     @GetMapping("/")
     public String index(@AuthenticationPrincipal Jwt jwt){
         return jwt.getClaimAsString("preferred_username");
@@ -18,5 +19,4 @@ public class Controller {
     public String premium(@AuthenticationPrincipal Jwt jwt){
         return "premium "+jwt.getClaimAsString("preferred_username");
     }
-
 }
