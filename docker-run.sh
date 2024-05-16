cf64b372db8a50b465927517769aab70016936d8
hwangjungpil <thinkhero@gmail.com>
2024-05-16 15:55:49 +0900

    id password encryption

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,8 @@
-docker run -d --name keycloak -p 8080:8080         -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin         -v /Users/mac/Documents/workspace/keycloakTest/data:/opt/keycloak/data --restart=unless-stopped         quay.io/keycloak/keycloak:latest         start-dev
+docker run -d --name keycloak -p 8080:8080        \
+	 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin        \
+	 -v /Users/mac/Documents/workspace/keycloakTest/data:/opt/keycloak/data \
+	 -v "/Users/mac/Documents/workspace/keycloakTest/theme/keycloakify-starter/dist_keycloak/target/keycloakify-starter-keycloak-theme-6.1.9.jar":"/opt/keycloak/providers/keycloakify-starter-keycloak-theme-6.1.9.jar" \
+   	 -v "/Users/mac/Documents/workspace/keycloakTest/theme/keycloakify-starter/dist_keycloak/src/main/resources/theme/account-v1":"/opt/keycloak/themes/account-v1":rw \
+   	 -v "/Users/mac/Documents/workspace/keycloakTest/theme/keycloakify-starter/dist_keycloak/src/main/resources/theme/keycloakify-starter":"/opt/keycloak/themes/keycloakify-starter":rw \
+   	 -v "/Users/mac/Documents/workspace/keycloakTest/theme/keycloakify-starter/dist_keycloak/src/main/resources/theme/keycloakify-starter_retrocompat":"/opt/keycloak/themes/keycloakify-starter_retrocompat":rw \
+	--restart=unless-stopped  quay.io/keycloak/keycloak:latest         start-dev
